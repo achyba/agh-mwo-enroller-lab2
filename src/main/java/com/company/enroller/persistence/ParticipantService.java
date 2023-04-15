accf13800bf46c9bf30a1dde18db1384a572b45e
student12 <student12@pcoip.ki.agh.edu.pl>
2023-04-15 10:25:40 +0200

    polowa zajec

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,6 +2,7 @@ package com.company.enroller.persistence;
 
 import com.company.enroller.model.Participant;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -18,6 +19,19 @@ public class ParticipantService {
 
 	public Collection<Participant> getAll() {
 		return connector.getSession().createCriteria(Participant.class).list();
+	}
+
+	public Collection<Participant> getAll(String sortBy, String sortOrder, String searchKey) {
+		String hql="FROM Participant WHERE login LIKE :key";
+		if (sortBy.equals("login")){
+			hql+=" ORDER BY  login";
+			if (sortOrder.equals("DESC")||sortOrder.equals("ASC")) {
+				hql += " " + sortOrder;
+			}
+		}
+		Query query = connector.getSession().createQuery(hql);
+		query.setParameter( "key", "%"+searchKey+"%");
+		return query.list();
 	}
 
 	public Participant findByLogin(String login) {
